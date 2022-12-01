d6adb73058c43ea5aaa53b1365a070adeb4ae5d1
rafael-takano <rafaelkuhntakano@hotmail.com>
2022-11-30 21:38:57 -0300

    Added the Model for the User

---
+++ src/database/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config()
+
+const userSchema = new mongoose.Schema({
+    login: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+    },
+    role: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+  })
+
+userSchema.methods.generateAuthToken = async () => {
+    const user =  this
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
+
+    return token
+}
+
+userSchema.statics.findByCredentials = async (login, password) => {
+    const user = await User.findOne({ login })
+    if (!user) return undefined
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) return undefined
+
+    return user
+}
+
+userSchema.pre('save', async (next) => {
+    const user = this
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password,8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
